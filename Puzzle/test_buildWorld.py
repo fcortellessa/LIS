de877a89074de12bb248f827b54511d3c33d50ac
Flavia Cortellessa <133102589+fcortellessa@users.noreply.github.com>
2024-03-10 18:16:49 +0100

    Update test_buildWorld.py
    added object oriented prebuilt puzzle

--- Puzzle/test_buildWorld.py
+++ Puzzle/test_buildWorld.py
@@ -1,10 +1,14 @@
 import robotic as ry
 import numpy as np
 import time
-from build_puzzleWorld2 import build_puzzleWorld
+# from prebuild_puzzleWorld import build_puzzleWorld
+import oop_prebuild_puzzleWorld as pWorld 
 
 
 C = ry.Config()
 C.addFile(ry.raiPath('scenarios/pandaSingle.g'))
-C = build_puzzleWorld(C)
+
+# C = build_puzzleWorld(C)
+puzzle_world = pWorld.PuzzleWorld(C)
+puzzle_world.build()
 C.view(True)
