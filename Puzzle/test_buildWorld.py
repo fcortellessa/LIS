4ea8fb30430e8793b07504397bd48503ca31306c
Flavia Cortellessa <133102589+fcortellessa@users.noreply.github.com>
2024-03-11 12:34:42 +0100

    Update test_buildWorld.py

--- Puzzle/test_buildWorld.py
+++ Puzzle/test_buildWorld.py
@@ -1,8 +1,7 @@
 import robotic as ry
-import numpy as np
-import time
-# from prebuild_puzzleWorld import build_puzzleWorld
-import oop_prebuild_puzzleWorld as pWorld 
+
+# from build_puzzleWorld2 import build_puzzleWorld
+import build_puzzleWorld3 as pWorld 
 
 
 C = ry.Config()
@@ -10,6 +9,8 @@ C.addFile(ry.raiPath('scenarios/pandaSingle.g'))
 
 # C = build_puzzleWorld(C)
 q_pWorld = [-0.2, 0.2, 0.05]
-puzzle_world = pWorld.PuzzleWorld(C, 'table', q_pWorld)
+q_start = [-4*0.04, 0.0, 0.015]
+q_goal = [-4*0.04, 4*0.04, 0.005]
+puzzle_world = pWorld.PuzzleWorld(C, 'table', q_pWorld, q_goal)
 puzzle_world.build()
 C.view(True)
